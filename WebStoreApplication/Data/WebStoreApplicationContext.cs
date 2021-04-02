e473fd10ee8207b52a85dae370ddb22debcf2726
Diana de Araujo Soares/RDiana.sD Support/S1/Samsung Electronics <diana.s@samsung.com>
2021-04-02 16:28:09 -0300

    departments CRUD created

---
+++ WebStoreApplication/Data/WebStoreApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebStoreApplication.Models;
+
+namespace WebStoreApplication.Data
+{
+    public class WebStoreApplicationContext : DbContext
+    {
+        public WebStoreApplicationContext (DbContextOptions<WebStoreApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebStoreApplication.Models.Department> Department { get; set; }
+    }
+}
