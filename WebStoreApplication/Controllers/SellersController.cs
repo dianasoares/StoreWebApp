961f317967dfff0c5d03dab4dc07285554d4f171
Diana de Araujo Soares/RDiana.sD Support/S1/Samsung Electronics <diana.s@samsung.com>
2021-04-14 00:58:44 -0300

    Sellers service added

--- WebStoreApplication/Controllers/SellersController.cs
+++ WebStoreApplication/Controllers/SellersController.cs
@@ -3,14 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStoreApplication.Services;
 
 namespace WebStoreApplication.Controllers
 {
     public class SellersController : Controller
     {
+        public readonly SellerService _sellerService;
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
---
+++ WebStoreApplication/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStoreApplication.Data;
+using WebStoreApplication.Models;
+
+namespace WebStoreApplication.Services
+{
+    public class SellerService 
+    {
+        private readonly WebStoreApplicationContext  _context;
+
+        public SellerService(WebStoreApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+
+        }
+
+    }
+}
